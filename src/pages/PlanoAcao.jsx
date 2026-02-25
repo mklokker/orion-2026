9a0d2ff2737ad2d4da4f400288161625f4846a9e
Marcos Souza <marcoslokker@gmail.com>
2026-02-25 20:28:20 +0000

    File changes

--- src/pages/PlanoAcao.jsx
+++ src/pages/PlanoAcao.jsx
@@ -416,6 +416,7 @@ export default function PlanoAcaoPage() {
         objectives={objectives}
         atas={atas}
         onUpdate={loadData}
+        onEdit={handleEditPlano}
       />
 
       <CadastrosModal
