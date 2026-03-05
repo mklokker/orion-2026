8bf93f89a290eeed7dd58eb284c82dbde14dcc1d
Marcos Souza <marcoslokker@gmail.com>
2026-03-05 13:39:06 +0000

    File changes

--- src/components/chat/MessageBubble.jsx
+++ src/components/chat/MessageBubble.jsx
@@ -1,4 +1,4 @@
-import React, { useState as useStateInner } from "react";
+import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { 
