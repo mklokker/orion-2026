e4b02b6508312b3bfc8e4e94f37e6d262b209c48
Marcos Souza <marcoslokker@gmail.com>
2026-03-05 12:54:14 +0000

    File changes

--- src/components/chat/ChatList.jsx
+++ src/components/chat/ChatList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
