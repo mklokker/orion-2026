af1b9fc77a2bc0cf456c6ade976c788148636c17
Marcos Souza <marcoslokker@gmail.com>
2026-03-06 15:25:04 +0000

    File changes

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1049,6 +1049,7 @@ export default function Chat() {
             onLoadMore={handleLoadMore}
             hasMoreMessages={hasMoreMessages}
             isLoadingMore={isLoadingMore}
+            autoFocusTrigger={selectedConversation?.id}
           />
         </div>
       </div>
